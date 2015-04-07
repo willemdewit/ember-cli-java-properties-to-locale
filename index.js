71e251146aa16c114abab10e081ddc34040a7469
Willem de Wit <willem_wit@hotmail.com>
2015-04-07 19:53:50 +0200

    fixing thingies

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
-var propertiesToObject = require('java.properties.js').default;
+var path      = require('path');
+var checker   = require('ember-cli-version-checker');
+var defaults  = require('lodash').defaults;
+
+var PropertiesPreprocessor = require('./lib/properties-preprocessor');
+
+module.exports = {
+  name: 'Ember CLI Properties Addon',
+
+  shouldSetupRegistryInIncluded: function() {
+    return !checker.isAbove(this, '0.2.0');
+  },
+
+  getConfig: function() {
+    var brocfileConfig = {};
+    var propsOptions = defaults(this.project.config(process.env.EMBER_ENV).propsOptions || {},
+      brocfileConfig, {
+        blueprints: true
+      });
+
+    return propsOptions;
+  },
+
+  blueprintsPath: function() {
+    if (this.getConfig().blueprints) {
+      return path.join(__dirname, 'blueprints');
+    }
+  },
+
+  setupPreprocessorRegistry: function(type, registry) {
+    var plugin = new PropertiesPreprocessor(this.getConfig());
+
+    registry.add('js', plugin);
+  },
+
+  included: function(app) {
+    this.app = app;
+
+    if (this.shouldSetupRegistryInIncluded()) {
+      this.setupPreprocessorRegistry('parent', app.registry);
+    }
+  }
+};
+/*
+var PropertiesFilter = require('broccoli-java-properties-to-object');
 var path         = require('path');
 var merge        = require('lodash-node/modern/objects/merge');
 var mergeTrees   = require('broccoli-merge-trees');
@@ -22,46 +66,43 @@ module.exports = {
 
   setupPreprocessorRegistry: function(type, registry) {
 	  var self = this;
-    registry.add('properties', {
+	  
+    registry.add('js', {
 	  name: 'ember-cli-java-properties-to-object',
 	  ext: 'properties',
 	  options: this.propertiesOptions(),
 	  toTree: function(tree, inputPath, outputPath, options) {
-		  console.log('toTree');
 		  options = merge({}, this.options, options);
 		  var ext = this.ext;
 		  var paths = options.outputPaths || { app: options.registry.app.options.outputPaths.app.properties };
-		  console.log(paths);
-		  Object.keys(paths).forEach(function (key) {
-			  console.log('..',key,paths[key]);
-		  }, this);
 
 		  var trees = Object.keys(paths).map(function(file) {
 		    var input = path.join(inputPath, file + '.' + ext);
 		    var output = paths[file];
-
-		    return propertiesToObject([tree], input, output, options);
+		    return PropertiesFilter([tree], input, output, options);
 		  });
 
 		  return mergeTrees(trees);
 		}
 	});
+    
+    if (type === 'parent') {
+        this.parentRegistry = registry;
+      }
   },
   
-  postprocessTree: function (type, tree) {
-    //console.log(tree);
-    return tree;
-  },
-
   isDevelopingAddon: function() {
     return true;
   },
   
   included: function(app) {
-    this.app = app;
+	  console.log('included');
+	  this.app = app; // used to provide back-compat for ember-cli < 0.2.0 in propertiesOptions()
+	  this._super.included.apply(this, arguments);
 
     if (this.shouldSetupRegistryInIncluded()) {
       this.setupPreprocessorRegistry('parent', app.registry);
     }
   }
 }
+*/
